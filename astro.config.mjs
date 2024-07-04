330f2cd84c7c3c670e55cd702ff479c121a05b70
Rodolpho Alves <rodolpho.castro.a+macosx@gmail.com>
2024-07-04 11:45:34 -0300

    chore: install React deps
    Signed-off-by: Rodolpho Alves <rodolpho.castro.a+macosx@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind()],
+  integrations: [solidJs(), tailwind(), react()]
 });
