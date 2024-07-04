7003501093e6d88a1b75fb4cb8d0b2762703a668
Rodolpho Alves <rodolpho.castro.a+macosx@gmail.com>
2024-07-04 13:11:30 -0300

    fix: remove unintended formatting
    Signed-off-by: Rodolpho Alves <rodolpho.castro.a+macosx@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind(), react()]
+  integrations: [solidJs(), tailwind()]
 });
