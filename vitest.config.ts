b671b2cee03e2866ed5020ecc4e55aa29c8118ae
Rodolpho Alves <rodolpho.castro.a+macosx@gmail.com>
2024-07-27 16:57:25 -0300

    fix: extend vitest properly
    Signed-off-by: Rodolpho Alves <rodolpho.castro.a+macosx@gmail.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,7 @@
 /// <reference types="vitest" />
-import { getViteConfig } from 'astro/config';
-import type { UserConfig as VitestUserConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config';
 
-// Type augmentation for Typescript to be aware of ViteTest configuration
-declare module 'vite' {
-  export interface UserConfig {
-    test: VitestUserConfig['test'];
-  }
-}
-
-export default getViteConfig({
+export default defineConfig({
   test: {
     // Vitest configuration options
     coverage: {
